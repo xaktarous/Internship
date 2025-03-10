5f03665032a105329b8c331159fb5a66f4e81cd1
xaktarous <delali.abdallah@gmail.com>
2025-03-10 12:53:55 +0100

    message

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 # another comment
 # another comment 3 
+# another comment 4
 urlpatterns = [
     path("list/",views.UserList.as_view()),
     path("profile/",views.UserDetail.as_view()),
